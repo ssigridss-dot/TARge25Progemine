54d960133be1091f64c7c4768f2f0fdbcfc0dc2e
Stenogon <135760807+Stenogon@users.noreply.github.com>
2026-09-07 15:28:59 +0300

    DbContext

---
+++ TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TARge25Shop.Data
+{
+    //nimetasime classi TARge25ShopContext, mis pärib DbContext klassi
+    public class TARge25ShopContext : DbContext
+    {
+
+        //see class tuleb teha DbContextiks,
+        //et saaks kasutada Entity Frameworki andmebaasi operatsioone
+        //tegime konteksti, mis pärib DbContext klassi
+        public TARge25ShopContext(DbContextOptions<TARge25ShopContext> options)
+            : base(options) { }
+    }
+}
