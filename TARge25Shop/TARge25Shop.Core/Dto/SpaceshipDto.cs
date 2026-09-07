631ac7903ab3e8465ae2cee74124d39d7361dcd3
Stenogon <135760807+Stenogon@users.noreply.github.com>
2026-09-07 16:18:54 +0300

    Dto update

--- TARge25Shop/TARge25Shop.Core/Dto/SpaceshipDto.cs
+++ TARge25Shop/TARge25Shop.Core/Dto/SpaceshipDto.cs
@@ -4,5 +4,12 @@ namespace TARge25Shop.Core.Dto
 {
     public class SpaceshipDto
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ShipType { get; set; } = string.Empty;
+        public int Crew { get; set; }
+        public int EnginePower { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
