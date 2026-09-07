9e5a3a6af2821cf1d4e50b72d82201401544db08
Stenogon <135760807+Stenogon@users.noreply.github.com>
2026-09-07 15:35:11 +0300

    DbContext, view

---
+++ TARge25Shop/TARge25Shop/Controllers/SpaceshipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TARge25Shop.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
